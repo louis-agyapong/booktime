ccd67cf4df1229794bce2dafdbe7abd6145470a7
Louis Agyapong <agyapong.louis@gmail.com>
2022-04-16 00:35:07 +0000

    test about view

--- booktime/main/tests.py
+++ booktime/main/tests.py
@@ -3,8 +3,14 @@ from http import HTTPStatus
 
 
 class TestCase(TestCase):
-    def test_home_page_works(self):
+    def test_home_page(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertTemplateUsed(response, "main/index.html")
         self.assertContains(response, "BookTime")
+
+    def test_about_page(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, "main/about.html")
+        self.assertContains(response, "About")
