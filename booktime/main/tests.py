07b8494c8e48825d0190cb3c93a3a4dce13c5940
Louis Agyapong <agyapong.louis@gmail.com>
2022-04-16 00:38:38 +0000

    using reverse in test instead of hard coded path

--- booktime/main/tests.py
+++ booktime/main/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from http import HTTPStatus
-
+from django.urls import reverse
 
 class TestCase(TestCase):
     def test_home_page(self):
@@ -10,7 +10,7 @@ class TestCase(TestCase):
         self.assertContains(response, "BookTime")
 
     def test_about_page(self):
-        response = self.client.get("/about/")
+        response = self.client.get(reverse("main:about"))
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertTemplateUsed(response, "main/about.html")
         self.assertContains(response, "About")
